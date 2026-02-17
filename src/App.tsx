75df4322a630f764a2192b0aa112e5c1c2577394
aleksi-svaridze <aleksi.svaridze82@gmail.com>
2026-02-18 00:48:28 +0400

    Add poto data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,49 @@
+import { useMemo, useState, useTransition, type ChangeEvent } from "react";
+import type { IPotos } from "./interfaces/potos.interfece";
+import potos from "./data/Potos";
+
 function App() {
-  return <>hello</>;
+  const [isPending, startTransition] = useTransition();
+  const [potoData, setPotoData] = useState<IPotos[]>(potos);
+  const [transitionedValue, setTransitionedValue] = useState("");
+  const [searchInputvalue, setSearchInputvalue] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchInputvalue(e.target.value);
+    startTransition(() => {
+      setTransitionedValue(e.target.value);
+    });
+  };
+
+  const letFilterPotoData = useMemo(() => {
+    console.log("RENDER");
+    return potos.filter((poto) => poto.title.includes(searchInputvalue));
+  }, [searchInputvalue]);
+
+  return (
+    <main className="flex px-10 gap-x-10">
+      <section className="w-2/3">
+        <h1 className="font-bold text-4xl">Content</h1>
+        <input
+          className="my-8 border outline-none w-full p-4"
+          type="text"
+          value={searchInputvalue}
+          onChange={(e) => handleChange(e)}
+        />
+        <div>
+          {letFilterPotoData.map((item) => (
+            <div className="border-b mb-4 flex" key={item.id}>
+              <img src={item.thumbnailUrl} className="w-28 h-28" />
+              <p>{item.title}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="w-1/3">
+        <h1 className="font-bold text-4xl mb-10">Form</h1>
+      </section>
+    </main>
+  );
 }
 
 export default App;
