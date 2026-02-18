0c0ed7cd3b225262c7afb131d47f9e7ff36a8d53
aleksi-svaridze <aleksi.svaridze82@gmail.com>
2026-02-18 10:36:17 +0400

    Add ShowData component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,12 @@
-import { useMemo, useState, useTransition, type ChangeEvent } from "react";
-import type { IPotos } from "./interfaces/potos.interfece";
-import potos from "./data/Potos";
 import ShowData from "./components/ShowData";
 import Form from "./components/Form";
 
 function App() {
-  const [isPending, startTransition] = useTransition();
-  const [potoData, setPotoData] = useState<IPotos[]>(potos);
-  const [transitionedValue, setTransitionedValue] = useState("");
-  const [searchInputvalue, setSearchInputvalue] = useState<string>("");
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchInputvalue(e.target.value);
-    startTransition(() => {
-      setTransitionedValue(e.target.value);
-    });
-  };
-
-  const letFilterPotoData = useMemo(() => {
-    console.log("RENDER");
-    return potos.filter((poto) => poto.title.includes(searchInputvalue));
-  }, [searchInputvalue]);
-
   return (
-    <main className="flex flex-col xl:flex-row h-screen">
+    <main className="flex flex-col xl:flex-row">
       <section className="w-full xl:w-2/3 bg-red-300  p-10 order-2">
         <h1 className="font-bold text-4xl">Content</h1>
-        <input
-          className="my-8 border outline-none w-full p-4"
-          type="text"
-          value={searchInputvalue}
-          onChange={(e) => handleChange(e)}
-        />
-        <div>
-          {/* {letFilterPotoData.map((poto, id) => (
-            <ShowData
-              title={poto.title}
-              key={id}
-              thumbnailUrl={poto.thumbnailUrl}
-            />
-          ))} */}
-        </div>
+        <ShowData />
       </section>
       <section className="w-full xl:w-1/3 bg-green-300 p-10 order-1">
         <h1 className="font-bold text-4xl">Form</h1>
--- src/components/ShowData.tsx
+++ src/components/ShowData.tsx
@@ -1,14 +1,23 @@
-interface Iprops {
-  thumbnailUrl: string;
-  title: string;
-}
+import { useState } from "react";
 
-function ShowData({ thumbnailUrl, title }: Iprops) {
+import potos from "../data/Potos";
+import type { IPotos } from "../interfaces/potos.interfece";
+
+function ShowData() {
+  const [potoData] = useState<IPotos[]>(potos);
+
   return (
-    <div className="border-b mb-4 flex">
-      <img src={thumbnailUrl} className="w-28 h-28" />
-      <p>{title}</p>
-    </div>
+    <>
+      <input className="my-8 border outline-none w-full p-4" type="text" />
+      <div>
+        {potoData.map(({ title, thumbnailUrl, id }) => (
+          <div className="border-b mb-4 flex" key={id}>
+            <img src={thumbnailUrl} className="w-28 h-28" />
+            <p>{title}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
